9a6fd9cf8595e4a6ac41299d82e89cca294cf102
Amine Benseddik <amine@qonto.eu>
2017-10-18 00:06:25 +0200

    inital commit

---
+++ goqonto.go
@@ -0,0 +1,172 @@
+package goqonto
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+
+	"github.com/amine7536/goqonto/context"
+)
+
+type Client struct {
+	client  *http.Client
+	BaseURL *url.URL
+
+	Organizations OrganizationsService
+	// Transactions  TransactionsService
+
+	// Optional function callback
+	onRequestCompleted RequestCompletionCallback
+}
+
+// RequestCompletionCallback defines the type of the request callback function
+type RequestCompletionCallback func(*http.Request, *http.Response)
+
+type Response struct {
+	*http.Response
+	Meta *ResponseMeta
+}
+
+type ResponseMeta struct {
+	CurrentPage int `json:"current_page,omiempty"`
+	NextPage    int `json:"next_page,omiempty"`
+	PrevPage    int `json:"prev_page,omiempty"`
+	TotalPages  int `json:"total_pages,omiempty"`
+	TotalCount  int `json:"total_count,omiempty"`
+	PerPage     int `json:"per_page,omiempty"`
+}
+
+// An ErrorResponse reports the error caused by an API request
+type ErrorResponse struct {
+	// HTTP response that caused this error
+	Response *http.Response
+
+	// Error message
+	Message string `json:"message"`
+}
+
+// ListOptions
+type ListOptions struct {
+	Page          int    `json:"page,omitempty"`
+	PerPage       int    `json:"per_page,omitempty"`
+	EnvironmentID string `json:"environment_id,omitempty"`
+}
+
+func New(httpClient *http.Client, apiUrl string) *Client {
+	if httpClient == nil {
+		httpClient = http.DefaultClient
+	}
+
+	baseURL, _ := url.Parse(apiUrl)
+
+	c := &Client{
+		client:  httpClient,
+		BaseURL: baseURL,
+	}
+	c.Organizations = &OrganizationsServiceOp{client: c}
+
+	return c
+}
+
+func (c *Client) NewRequest(ctx context.Context, method, urlStr string, body interface{}) (*http.Request, error) {
+	rel, err := url.Parse(urlStr)
+	if err != nil {
+		return nil, err
+	}
+
+	u := c.BaseURL.ResolveReference(rel)
+
+	buf := new(bytes.Buffer)
+	if body != nil {
+		err = json.NewEncoder(buf).Encode(body)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	req, err := http.NewRequest(method, u.String(), buf)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Add("content-type", "application/json")
+
+	return req, nil
+}
+
+func newResponse(r *http.Response) *Response {
+	response := &Response{
+		Response: r,
+	}
+	return response
+}
+
+// Do sends an API request and returns the API response. The API response is JSON decoded and stored in the value
+// pointed to by v, or returned as an error if an API error has occurred. If v implements the io.Writer interface,
+// the raw response will be written to v, without attempting to decode it.
+func (c *Client) Do(ctx context.Context, req *http.Request, v interface{}) (*Response, error) {
+	resp, err := context.DoRequestWithClient(ctx, c.client, req)
+	if err != nil {
+		return nil, err
+	}
+	if c.onRequestCompleted != nil {
+		c.onRequestCompleted(req, resp)
+	}
+
+	defer func() {
+		if rerr := resp.Body.Close(); err == nil {
+			err = rerr
+		}
+	}()
+
+	response := newResponse(resp)
+
+	err = CheckResponse(resp)
+	if err != nil {
+		return response, err
+	}
+
+	if v != nil {
+		if w, ok := v.(io.Writer); ok {
+			_, err = io.Copy(w, resp.Body)
+			if err != nil {
+				return nil, err
+			}
+		} else {
+			err = json.NewDecoder(resp.Body).Decode(v)
+			if err != nil {
+				return nil, err
+			}
+		}
+	}
+
+	return response, err
+}
+
+// CheckResponse checks the API response for errors, and returns them if present. A response is considered an
+// error if it has a status code outside the 200 range. API error responses are expected to have either no response
+// body, or a JSON response body that maps to ErrorResponse. Any other response body will be silently ignored.
+func CheckResponse(r *http.Response) error {
+	if c := r.StatusCode; c >= 200 && c <= 299 {
+		return nil
+	}
+
+	errorResponse := &ErrorResponse{Response: r}
+	data, err := ioutil.ReadAll(r.Body)
+	if err == nil && len(data) > 0 {
+		err := json.Unmarshal(data, errorResponse)
+		if err != nil {
+			return err
+		}
+	}
+
+	return errorResponse
+}
+
+func (r *ErrorResponse) Error() string {
+	return fmt.Sprintf("%v %v: %d %v", r.Response.Request.Method, r.Response.Request.URL, r.Response.StatusCode, r.Message)
+}
---
+++ organizations.go
@@ -0,0 +1,84 @@
+package goqonto
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/amine7536/goqonto/context"
+)
+
+const organizationsBasePath = "v1/organizations"
+
+// OrganizationsService Service
+type OrganizationsService interface {
+	Get(context.Context, string) (*Organization, *Response, error)
+}
+
+// Organization struct
+// Reflects :
+// {
+// 	"organization": {
+// 		"slug": "croissant-9134",
+// 		"bank_accounts": [
+// 			{
+// 				"slug": "croissant-bank-account-1",
+// 				"iban": "FR7616798000010000004321396",
+// 				"bic": "TRZOFR21XXX",
+// 				"currency": "EUR",
+// 				"balance": 24.94
+// 			}
+// 		]
+// 	}
+// }
+type Organization struct {
+	Slug         string        `json:"slug"`
+	BankAccounts []BankAccount `json:"bank_accounts"`
+}
+
+// BankAccount struct
+type BankAccount struct {
+	Slug     string  `json:"slug"`
+	IBAN     string  `json:"iban"`
+	BIC      string  `json:"bic"`
+	Currency string  `json:"currency"`
+	Balance  float32 `json:"balance"`
+}
+
+// OrganizationsServiceOp Service
+type OrganizationsServiceOp struct {
+	client *Client
+}
+
+var _ OrganizationsService = &OrganizationsServiceOp{}
+
+type organizationsRoot struct {
+	Organization Organization `json:"organization"`
+}
+
+// Convert Organization to a string
+func (o Organization) String() string {
+	bytes, err := json.Marshal(o)
+	if err != nil {
+		panic(err)
+	}
+	return string(bytes)
+}
+
+// Get Organizations
+func (s *OrganizationsServiceOp) Get(ctx context.Context, id string) (*Organization, *Response, error) {
+
+	path := fmt.Sprintf("%s/%s", organizationsBasePath, id)
+
+	req, err := s.client.NewRequest(ctx, "GET", path, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	root := new(organizationsRoot)
+	resp, err := s.client.Do(ctx, req, root)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return &root.Organization, resp, nil
+}
