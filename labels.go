345b442886ff5bf9ff98828fa42c7275168280ff
Amine Benseddik <amine@qonto.eu>
2019-06-14 18:14:36 +0200

    feat(labels): Add support for Labels

---
+++ labels.go
@@ -0,0 +1,68 @@
+package goqonto
+
+import (
+	"context"
+	"net/http"
+)
+
+// labelsBasePath Qonto API Labels Endpoint
+const labelsBasePath = "v2/labels"
+
+// LabelsOptions Qonto API Labels query strings
+// https://api-doc.qonto.eu/2.0/labels/list-labels
+type LabelsOptions struct {
+	CurrentPage int64 `json:"current_page,omitempty"`
+	PerPage     int64 `json:"per_page,omitempty"`
+}
+
+// LabelsService interface
+// List: list all the labels
+type LabelsService interface {
+	List(context.Context, *LabelsOptions) ([]Label, *Response, error)
+}
+
+// Label struct
+// https://api-doc.qonto.eu/2.0/labels/list-labels
+type Label struct {
+	ID       string `json:"id"`
+	Name     string `json:"name"`
+	ParentID string `json:"parent_id"`
+}
+
+// LabelsServiceOp struct used to embed *Client
+type LabelsServiceOp struct {
+	client *Client
+}
+
+var _ LabelsService = &LabelsServiceOp{}
+
+// labelsRoot root key in the JSON response for labels
+type labelsRoot struct {
+	Labels []Label `json:"labels"`
+}
+
+// List all the labels
+func (m *LabelsServiceOp) List(ctx context.Context, memOpt *LabelsOptions) ([]Label, *Response, error) {
+
+	req, err := m.client.NewRequest(ctx, http.MethodGet, labelsBasePath, memOpt)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	type respWithMeta struct {
+		labelsRoot
+		metaRoot
+	}
+
+	root := new(respWithMeta)
+	resp, err := m.client.Do(ctx, req, root)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	if m := &root.metaRoot; m != nil {
+		resp.Meta = &m.Meta
+	}
+
+	return root.Labels, resp, nil
+}
