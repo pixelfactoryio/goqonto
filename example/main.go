09cf12095f3363af29ce1ac14c37e76ba37987aa
Amine Benseddik <amine.benseddik@gmail.com>
2020-11-25 18:42:21 +0100

    Use thirdparty.qonto.com as default baseURL (#23)
    * docs: update README
    
    * docs: add example
    
    * build: use go 1.15
    
    * fix: use thirdparty.qonto.com by default
    
    * fix: lint example
    
    * build: drop go 1.12
    
    Co-authored-by: Amine Benseddik <amine@pixelfactory.io>

---
+++ example/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/pixelfactoryio/goqonto/v2"
+)
+
+// AuthTransport structs holds company Slug and  Secret key
+type AuthTransport struct {
+	*http.Transport
+	Slug   string
+	Secret string
+}
+
+// RoundTrip set "Authorization" header
+func (t AuthTransport) RoundTrip(r *http.Request) (*http.Response, error) {
+	r.Header.Set("Authorization", fmt.Sprintf("%s:%s", t.Slug, t.Secret))
+	return t.Transport.RoundTrip(r)
+}
+
+func main() {
+	orgID := os.Getenv("QONTO_ORG_ID")
+	userLogin := os.Getenv("QONTO_USER_LOGIN")
+	userSecretKey := os.Getenv("QONTO_SECRET_KEY")
+
+	client := http.Client{
+		Transport: AuthTransport{
+			&http.Transport{},
+			userLogin,
+			userSecretKey,
+		},
+	}
+
+	qonto := goqonto.NewClient(&client)
+	ctx := context.Background()
+
+	// Get Organisation
+	orga, resp, err := qonto.Organizations.Get(ctx, orgID)
+	if err != nil && resp.StatusCode != http.StatusOK {
+		panic(err.Error())
+	}
+	prettyPrint(orga)
+
+	// List Transactions
+	params := &goqonto.TransactionsOptions{
+		Slug:   orga.Slug,
+		IBAN:   orga.BankAccounts[0].IBAN,
+		Status: []string{"completed"},
+	}
+
+	transactions, resp, err := qonto.Transactions.List(ctx, params)
+	if err != nil && resp.StatusCode != http.StatusOK {
+		panic(err.Error())
+	}
+
+	for _, trx := range transactions {
+		prettyPrint(trx)
+	}
+	prettyPrint(resp.Meta)
+
+}
+
+func prettyPrint(v interface{}) {
+	b, _ := json.MarshalIndent(v, "", "  ")
+	fmt.Println(string(b))
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/pixelfactoryio/goqonto/v2
 
-go 1.14
+go 1.15
