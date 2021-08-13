7ce5b5d326f10b667afc98193c4c9e0314a56011
Amine Benseddik <amine@pixelfactory.io>
2021-08-13 19:38:29 +0200

    Feat get transaction by id (#28)
    * fix typo
    
    * feat(transactions): add transactions.Get
    
    Add the ability to get a transactions by id.
    
    * update linter configuration
    
    * fix linter issue
    
    * use latest golangci-lint version

--- transactions_test.go
+++ transactions_test.go
@@ -9,50 +9,8 @@ import (
 )
 
 var (
-	transactionsFixture = `{
-		"transactions": 
-		[
-			{
-				"transaction_id": "mycompany-bank-account-1-transaction-491",
-				"amount": 126.0,
-				"amount_cents": 12600,
-				"attachment_ids": [],
-				"local_amount": 126.0,
-				"local_amount_cents": 12600,
-				"side": "debit",
-				"operation_type": "transfer",
-				"currency": "EUR",
-				"local_currency": "EUR",
-				"label": "Qonto",
-				"settled_at": "2019-05-29T05:28:00.191Z",
-				"emitted_at": "2019-05-29T05:27:51.353Z",
-				"updated_at": "2019-05-29T05:29:38.068Z",
-				"status": "completed",
-				"note": "Memo added by the user on the transaction",
-				"reference": "Message sent along income, transfer and direct_debit transactions",
-				"vat_amount": 21.0,
-				"vat_amount_cents": 2100,
-				"vat_rate": 20.0,
-				"initiator_id": "ID of the membership who initiated the transaction",
-				"label_ids": [
-					"f4c39147-9c1f-43b0-4720-bd6ef6ac372d",
-					"dee2cfdb-8147-444c-9b8d-5c0aa25b8dd9"
-				],
-				"attachment_lost": false,
-				"attachment_required": true
-			}
-		],
-		"meta": {
-			"current_page": 2,
-			"next_page": 3,
-			"prev_page": 1,
-			"total_pages": 3,
-			"total_count": 30,
-			"per_page": 10
-		}
-	}`
-
 	transactionFixture = `{
+		"id": "6ea8271c-87b1-49d0-a66f-1e29a2fe43ba",
 		"transaction_id": "mycompany-bank-account-1-transaction-491",
 		"amount": 126.0,
 		"amount_cents": 12600,
@@ -82,10 +40,30 @@ var (
 		"attachment_required": true
 	}`
 
+	metaFixture = `"meta": {
+		"current_page": 2,
+		"next_page": 3,
+		"prev_page": 1,
+		"total_pages": 3,
+		"total_count": 30,
+		"per_page": 10
+	}`
+
+	listTransactionsFixture = fmt.Sprintf(`{
+		"transactions": [ %s ],
+		%s
+
+	}`, transactionFixture, metaFixture)
+
+	getTransactionFixture = fmt.Sprintf(`{
+		"transaction": %s 
+	}`, transactionFixture)
+
 	trx1SettledAt, _ = time.Parse(time.RFC3339, "2019-05-29T05:28:00.191Z")
 	trx1EmittedAt, _ = time.Parse(time.RFC3339, "2019-05-29T05:27:51.353Z")
 	trx1UpdatedAt, _ = time.Parse(time.RFC3339, "2019-05-29T05:29:38.068Z")
 	trx1             = Transaction{
+		ID:               "6ea8271c-87b1-49d0-a66f-1e29a2fe43ba",
 		TransactionID:    "mycompany-bank-account-1-transaction-491",
 		Amount:           126.0,
 		AmountCents:      12600,
@@ -123,7 +101,6 @@ var (
 func TestTransaction_marshall(t *testing.T) {
 	testJSONMarshal(t, &Transaction{}, "{}")
 	testJSONMarshal(t, &trx1, transactionFixture)
-
 }
 
 func TestTransactionsService_List(t *testing.T) {
@@ -135,7 +112,7 @@ func TestTransactionsService_List(t *testing.T) {
 		testHeader(t, r, "Accept", mediaType)
 		testHeader(t, r, "Content-Type", mediaType)
 		testBody(t, r, `{"slug":"mycompany-9134","iban":"FR761679800001000000123456"}`+"\n")
-		fmt.Fprint(w, transactionsFixture)
+		fmt.Fprint(w, listTransactionsFixture)
 	})
 
 	params := &TransactionsOptions{
@@ -178,6 +155,59 @@ func TestTransactionsService_List_Error(t *testing.T) {
 	})
 
 	got, resp, err := client.Transactions.List(ctx, &TransactionsOptions{})
+
+	if err.Error() == "" {
+		t.Errorf("Expected non-empty err.Error()")
+	}
+
+	if resp.StatusCode != http.StatusNotFound {
+		t.Errorf("Expected 404 Status")
+	}
+
+	if got != nil {
+		t.Errorf("Expected empty body")
+	}
+}
+
+func TestTransactionsService_Get(t *testing.T) {
+	setup()
+	defer teardown()
+
+	mux.HandleFunc(
+		fmt.Sprintf("/%s/6ea8271c-87b1-49d0-a66f-1e29a2fe43ba", transactionsBasePath),
+		func(w http.ResponseWriter, r *http.Request) {
+			testMethod(t, r, http.MethodGet)
+			testHeader(t, r, "Accept", mediaType)
+			testHeader(t, r, "Content-Type", mediaType)
+			fmt.Fprint(w, getTransactionFixture)
+		})
+
+	got, _, err := client.Transactions.Get(ctx, "6ea8271c-87b1-49d0-a66f-1e29a2fe43ba")
+	if err != nil {
+		t.Errorf("Transactions.Get returned error: %v", err)
+	}
+
+	want := &trx1
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("Transactions.Get \n got %v\n want %v\n", got, want)
+	}
+
+}
+
+func TestTransactionsService_Get_Error(t *testing.T) {
+	setup()
+	defer teardown()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, http.MethodGet)
+		testHeader(t, r, "Accept", mediaType)
+		testHeader(t, r, "Content-Type", mediaType)
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(w, `{ "message": "Not found" }`)
+	})
+
+	got, resp, err := client.Transactions.Get(ctx, "6ea8271c-87b1-49d0-a66f-1e29a2fe43ba")
 
 	if err.Error() == "" {
 		t.Errorf("Expected non-empty err.Error()")
